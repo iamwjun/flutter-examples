343ecc67374462b91617c097502e97d3f24b6a53
wujun <cddxwujun@qq.com>
2019-05-07 14:28:08 +0800

    Test FlutterLoginPageBloc and success

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:myapp/bloc.dart';
+import 'package:myapp/pagetwo.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      theme: new ThemeData(primarySwatch: Colors.blue),
-      home: new LoginPage(),
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class LoginPage extends StatefulWidget {
-  @override
-  State createState() => new LoginPageState();
-}
-
-class LoginPageState extends State<LoginPage>
-    with SingleTickerProviderStateMixin {
-  Animation<double> _iconAnimation;
-  AnimationController _iconAnimationController;
-
-  @override
-  void initState() {
-    super.initState();
-    _iconAnimationController = new AnimationController(
-        vsync: this, duration: new Duration(milliseconds: 500));
-    _iconAnimation = new CurvedAnimation(
-      parent: _iconAnimationController,
-      curve: Curves.bounceOut,
-    );
-    _iconAnimation.addListener(() => this.setState(() {}));
-    _iconAnimationController.forward();
+class HomePage extends StatelessWidget {
+  changeThePage(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => PageTwo()));
   }
 
   @override
   Widget build(BuildContext context) {
-    
-    return new Scaffold(
-      backgroundColor: Colors.white,
-      body: new Stack(fit: StackFit.expand, children: <Widget>[
-        new Image(
-          image: new AssetImage("assets/girl.jpeg"),
-          fit: BoxFit.cover,
-          colorBlendMode: BlendMode.darken,
-          color: Colors.black87,
-        ),
-        new Theme(
-          data: new ThemeData(
-              brightness: Brightness.dark,
-              inputDecorationTheme: new InputDecorationTheme(
-                // hintStyle: new TextStyle(color: Colors.blue, fontSize: 20.0),
-                labelStyle:
-                    new TextStyle(color: Colors.tealAccent, fontSize: 25.0),
-              )),
-          isMaterialAppTheme: true,
-          child: new Column(
+    final bloc = Bloc();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bloc Pattern"),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              new FlutterLogo(
-                size: _iconAnimation.value * 140.0,
+              StreamBuilder<String>(
+                stream: bloc.email,
+                builder: (context, snapshot) => TextField(
+                      onChanged: bloc.emailChanged,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Enter email",
+                          labelText: "Email",
+                          errorText: snapshot.error),
+                    ),
               ),
-              new Container(
-                padding: const EdgeInsets.all(40.0),
-                child: new Form(
-                  autovalidate: true,
-                  child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      new TextFormField(
-                        decoration: new InputDecoration(
-                            labelText: "Enter Email", fillColor: Colors.white),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                      new TextFormField(
-                        decoration: new InputDecoration(
-                          labelText: "Enter2 Password",
-                        ),
-                        obscureText: true,
-                        keyboardType: TextInputType.text,
-                      ),
-                      new Padding(
-                        padding: const EdgeInsets.only(top: 60.0),
-                      ),
-                      new MaterialButton(
-                        height: 50.0,
-                        minWidth: 150.0,
-                        color: Colors.green,
-                        splashColor: Colors.teal,
-                        textColor: Colors.white,
-                        child: new Icon(FontAwesomeIcons.signInAlt),
-                        onPressed: () {},
-                      )
-                    ],
-                  ),
-                ),
-              )
+              SizedBox(
+                height: 20.0,
+              ),
+              StreamBuilder<String>(
+                stream: bloc.password,
+                builder: (context, snapshot) => TextField(
+                      onChanged: bloc.passwordChanged,
+                      keyboardType: TextInputType.text,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Enter password",
+                          labelText: "Password",
+                          errorText: snapshot.error),
+                    ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              StreamBuilder<bool>(
+                stream: bloc.submitCheck,
+                builder: (context, snapshot) => RaisedButton(
+                      color: Colors.tealAccent,
+                      onPressed: snapshot.hasData
+                          ? () => changeThePage(context)
+                          : null,
+                      child: Text("Submit"),
+                    ),
+              ),
             ],
           ),
         ),
-      ]),
+      ),
     );
   }
 }
