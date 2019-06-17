881ba202cd4bdb1b6bcbd43258b4c2e44beb63dc
wujun <cddxwujun@qq.com>
2019-06-17 16:18:16 +0800

    Combing project entry files main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/pages/Quiz/scorePage.dart';
-// import 'package:myapp/pages/Signature/home.dart';
-// import 'package:myapp/pages/Charts/home.dart';
+/*
+ * 模仿Instagram APP示例
+ */
 // import 'package:myapp/pages/Instagram/home.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Instagram',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//           primarySwatch: Colors.blue,
+//           primaryColor: Colors.black,
+//           primaryIconTheme: IconThemeData(color: Colors.black),
+//           primaryTextTheme: TextTheme(
+//               title: TextStyle(color: Colors.black, fontFamily: "Aveny")),
+//           textTheme: TextTheme(title: TextStyle(color: Colors.black))),
+//       home: InstagramHome(),
+//     );
+//   }
+// }
+
+/*
+ * AvailableKittens APP示例
+ */
 // import 'package:myapp/pages/AvailableKittens/AvailableKittens.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: AvailableKittens(),
+//     );
+//   }
+// }
+
+/*
+ * 图表APP示例
+ * 参考: https://google.github.io/charts/flutter/gallery.html
+ */
+// import 'package:myapp/pages/Charts/home.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(  
+//       theme: ThemeData(
+//           primarySwatch: Colors.purple,
+//           cardColor: Colors.purple,
+//           buttonColor: Colors.purple,
+//           buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary)),
+//       home: PieChart(),
+//     );
+//   }
+// }
+
+/*
+ * 签名APP示例
+ */
+// import 'package:myapp/pages/Signature/home.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: SignatureHome(),
+//     );
+//   }
+// }
+
+/*
+ * 简单的答题APP示例
+ */
 import './pages/Quiz/landingPage.dart';
-import './pages/Quiz/quizPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,21 +89,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // title: 'Instagram',
-      // debugShowCheckedModeBanner: false,
-      // theme: ThemeData(
-      //     primarySwatch: Colors.blue,
-      //     primaryColor: Colors.black,
-      //     primaryIconTheme: IconThemeData(color: Colors.black),
-      //     primaryTextTheme: TextTheme(
-      //         title: TextStyle(color: Colors.black, fontFamily: "Aveny")),
-      //     textTheme: TextTheme(title: TextStyle(color: Colors.black))),    
-      // theme: ThemeData(
-      //     primarySwatch: Colors.purple,
-      //     cardColor: Colors.purple,
-      //     buttonColor: Colors.purple,
-      //     buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary)),
       home: LandingPage(),
     );
   }
 }
+
+
+
+
+
+
