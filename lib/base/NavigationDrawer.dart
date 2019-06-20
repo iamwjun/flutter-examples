978e625cbb27d8d5e44fce32473fac3500dbc870
wujun <cddxwujun@qq.com>
2019-06-20 18:22:18 +0800

    Start NavigationDrawer learning

---
+++ lib/base/NavigationDrawer.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NavigationDrawer extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new NavigateionDrawerState();
+}
+
+class NavigateionDrawerState extends State<NavigationDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }  
+}
