d910e05eb55b2b70e4bf205a0cf0e29f8557db6d
wujun <cddxwujun@qq.com>
2019-06-28 17:29:04 +0800

    Try using alipay aar sdk

--- android/app/src/main/java/com/example/example/MainActivity.java
+++ android/app/src/main/java/com/example/example/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.BatteryManager;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
-import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.plugin.common.MethodCall;
